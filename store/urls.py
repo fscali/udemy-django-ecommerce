a96404a705dc7a0a6e2b0d457e7cc61a54faea65
fscali <francesco.scali@gmail.com>
2022-01-20 07:57:45 +0100

    filtering store by categories

--- store/urls.py
+++ store/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.StoreView.as_view()),
+    path('', views.StoreView.as_view(), name='store'),
+    path('categories/<slug:category_name>',
+         views.StoreView.as_view(), name='store-categories')
     #path('', views.home, name="home")
 ]
--- store/views.py
+++ store/views.py
@@ -1,17 +1,33 @@
 from itertools import product
+from unicodedata import category
 from django.shortcuts import render
 from django.views.generic import View
 
 from .models import Product
 
+from category.models import Category
+
+
 # Create your views here.
 
 
 class StoreView(View):
-    def get(self, request):
+  # Story.objects.get(slug=self.kwargs['slug'])
+    def get(self, request, category_name=''):
         products = Product.objects.all().filter(is_available=True)
         products_count = products.count()
+        selected_category = None
+
+        #category_name = self.kwargs.get('category_name')
+
+        if category_name:
+            selected_category = Category.objects.get(slug=category_name)
+            products = products.filter(category=selected_category)
+
+        categories = Category.objects.all()
         return render(request, 'store/store.html', {
             'products': products,
-            'products_count': products_count
+            'products_count': products_count,
+            'categories': categories,
+            'selected_category': selected_category
         })
