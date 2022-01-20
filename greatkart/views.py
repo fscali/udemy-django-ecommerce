176164f04e40c664d09a39c039fbebed63d381ec
fscali <francesco.scali@gmail.com>
2022-01-20 06:15:34 +0100

    dynamic home with products

--- greatkart/views.py
+++ greatkart/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+
+from store.models import Product
 
 
-def home(request):
+class HomeView(ListView):
+    queryset = Product.objects.filter(is_available=True)
+    model = Product
+    context_object_name = 'products'
+    template_name = 'home.html'
+
+
+""" def home(request):
     return render(request, 'home.html')
+ """
---
+++ store/templatetags/store_extras.py
@@ -0,0 +1,14 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter
+def addstr(arg1, arg2):
+    """concatenate arg1 & arg2"""
+    return str(arg1) + str(arg2)
+
+
+@register.filter
+def currency(amount):
+    return f'{amount:.2f} $'
