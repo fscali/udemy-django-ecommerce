3e06bf961e23d6c2190a8394229d859b8e8ec71e
fscali <francesco.scali@gmail.com>
2022-01-21 04:17:27 +0100

    category context processors + categories in dpd menu

--- store/views.py
+++ store/views.py
@@ -12,22 +12,17 @@ from category.models import Category
 
 
 class StoreView(View):
-  # Story.objects.get(slug=self.kwargs['slug'])
     def get(self, request, category_name=''):
         products = Product.objects.all().filter(is_available=True)
         products_count = products.count()
         selected_category = None
 
-        #category_name = self.kwargs.get('category_name')
-
         if category_name:
             selected_category = Category.objects.get(slug=category_name)
             products = products.filter(category=selected_category)
 
-        categories = Category.objects.all()
         return render(request, 'store/store.html', {
             'products': products,
             'products_count': products_count,
-            'categories': categories,
             'selected_category': selected_category
         })
